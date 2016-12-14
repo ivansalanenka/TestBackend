b889a5bfbd98def9b1a1e5dd67789b6ac8860dfb
Pavel Hetsman <kv4sha@gmail.com>
2016-12-14 13:17:58 +0300

    Add file upload controller

---
+++ Owin/Owin.Server/Controllers/FileUploadController.cs
@@ -0,0 +1,106 @@
+﻿using Owin.Server.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+namespace Owin.Server.Controllers
+{
+    public class FileUploadController : ApiController
+    {
+        public string Post()
+        {
+            var contents = ParseContent(Request.Content);
+            var file = (byte[])contents["file"]["File"];
+
+            if (!Directory.Exists("ui"))
+            {
+                Directory.CreateDirectory("ui");
+            }
+
+            if (!Directory.Exists("ui/files"))
+            {
+                Directory.CreateDirectory("ui/files");
+            }
+
+            using (var fileStream = File.Create($"ui/files/{contents["file"]["FileName"]}"))
+            {
+                fileStream.Write(file, 0, file.Length);
+            }
+
+            return $"files/{contents["file"]["FileName"]}";
+        }
+
+        public static Dictionary<string, Dictionary<string, object>> ParseContent(HttpContent content)
+        {
+            var provider = new MultipartMemoryStreamProvider();
+            var httpContentList = content.ReadAsMultipartAsync(provider).
+              ContinueWith(o => provider.Contents.ToList()).Result;
+
+            if (httpContentList.Count == 0)
+            {
+                throw new HttpResponseException(HttpStatusCode.NoContent);
+            }
+
+            var contents = new Dictionary<string, Dictionary<string, object>>();
+
+            foreach (var httpContent in httpContentList)
+            {
+                var contentDispositionName = UnquoteToken(httpContent.Headers.ContentDisposition.Name);
+
+                if (!string.IsNullOrWhiteSpace(httpContent.Headers.ContentDisposition.FileName))
+                {
+                    var bytes = GetBytes(httpContent);
+                    contents.Add(contentDispositionName, new Dictionary<string, object>
+            {
+              {"File", bytes},
+              {"FileName", UnquoteToken(httpContent.Headers.ContentDisposition.FileName)},
+              {"MediaType", httpContent.Headers.ContentType.MediaType}
+            });
+                }
+                else
+                {
+                    var text = Encoding.UTF8.GetString(GetBytes(httpContent));
+                    contents.Add(contentDispositionName, new Dictionary<string, object>
+            {
+              {"Text", text}
+            });
+                }
+            }
+
+            return contents;
+        }
+
+        public static string UTF8ToUnicode(string input)
+        {
+            return Encoding.Unicode.GetString(Encoding.Convert(Encoding.UTF8, Encoding.Unicode, Encoding.UTF8.GetBytes(input)));
+        }
+
+        private static byte[] GetBytes(HttpContent content)
+        {
+            var inputStream = content.ReadAsStreamAsync().Result;
+            var outputStream = new MemoryStream();
+
+            inputStream.CopyTo(outputStream);
+            return outputStream.ToArray();
+        }
+
+        private static string UnquoteToken(string token)
+        {
+            if (string.IsNullOrWhiteSpace(token))
+                return token;
+
+            if (token.StartsWith("\"", StringComparison.Ordinal) && token.EndsWith("\"", StringComparison.Ordinal) && token.Length > 1)
+                return token.Substring(1, token.Length - 2);
+
+            return token;
+        }
+    }
+
+}
--- Owin/Owin.Server/Startup.cs
+++ Owin/Owin.Server/Startup.cs
@@ -18,42 +18,45 @@ namespace Owin.Server
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
             HttpConfiguration config = new HttpConfiguration();
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
+                name: "DefaultJsonApi",
                 routeTemplate: "api/v1.1/{controller}.json/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/v1.1/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
             config.Formatters.Remove(config.Formatters.XmlFormatter);
 
             app.UseWebApi(config);
 
-            if (bool.Parse(ConfigurationManager.AppSettings["ServeUI"]) == true)
+            app.Map("", spa =>
             {
-                app.Map("", spa =>
+                spa.Use((context, next) =>
                 {
-                    spa.Use((context, next) =>
-                    {
-                        if (context.Request.Path.ToUriComponent().Contains(".js")) { return next(); }
-                        if (context.Request.Path.ToUriComponent().Contains(@"/img/")) { return next(); }
-                        if (context.Request.Path.ToUriComponent().Contains(@"/fonts/")) { return next(); }
-                        if (context.Request.Path.ToUriComponent().Contains(@"/css/")) { return next(); }
+                    if (context.Request.Path.ToUriComponent().Contains(".js")) { return next(); }
+                    if (context.Request.Path.ToUriComponent().Contains(@"/img/")) { return next(); }
+                    if (context.Request.Path.ToUriComponent().Contains(@"/fonts/")) { return next(); }
+                    if (context.Request.Path.ToUriComponent().Contains(@"/css/")) { return next(); }
+                    if (context.Request.Path.ToUriComponent().Contains(@"/files/")) { return next(); }
 
-                        context.Request.Path = new PathString("/");
+                    context.Request.Path = new PathString("/");
 
-                        return next();
-                    });
-
-                    var physicalFileSystem = new PhysicalFileSystem(ConfigurationManager.AppSettings["ClientFolder"]);
-                    var options = new FileServerOptions
-                    {
-                        EnableDefaultFiles = true,
-                        FileSystem = physicalFileSystem
-                    };
-                    options.StaticFileOptions.FileSystem = physicalFileSystem;
-                    options.StaticFileOptions.ServeUnknownFileTypes = true;
-                    options.DefaultFilesOptions.DefaultFileNames = new[] { "index.html" };
-                    spa.UseFileServer(options);
+                    return next();
                 });
-            }
+
+                var physicalFileSystem = new PhysicalFileSystem(ConfigurationManager.AppSettings["ClientFolder"]);
+                var options = new FileServerOptions
+                {
+                    EnableDefaultFiles = true,
+                    FileSystem = physicalFileSystem
+                };
+                options.StaticFileOptions.FileSystem = physicalFileSystem;
+                options.StaticFileOptions.ServeUnknownFileTypes = true;
+                options.DefaultFilesOptions.DefaultFileNames = new[] { "index.html" };
+                spa.UseFileServer(options);
+            });
         }
     }
 }
